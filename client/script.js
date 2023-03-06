2313878e7a72c43e38d64907a861adb62d51b19d
nawabsahab16 <117763400+nawabsahab16@users.noreply.github.com>
2023-03-06 21:11:52 +0530

    Handle events with JS

---
+++ client/script.js
@@ -0,0 +1,19 @@
+import bot from './assets/bot.svg';
+import user from './assets/user.svg';
+
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
+
+let valoadInterval;
+
+function loader(element) {
+    element.textContent = '';
+
+    loadInterval = setInterval(() => {
+        element.textContainer += '.' ;
+
+        if(element.textContainer === '....') {
+            element.textContainer = '';
+        }
+    }, 300)
+}

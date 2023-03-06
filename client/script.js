11684790a334fc771fb1f77743db772fb87609e6
nawabsahab16 <117763400+nawabsahab16@users.noreply.github.com>
2023-03-07 00:37:11 +0530

    js function to loader, typetext and much more

--- client/script.js
+++ client/script.js
@@ -17,3 +17,46 @@ function loader(element) {
         }
     }, 300)
 }
+
+function typeText(element, text) {
+    let index=0;
+
+    let interval = setInterval(()=> {
+
+        if(index < text.length) {
+            element.innerHTML += text.chartAt(index);
+            index++; 
+        }
+        else {
+            clearInterval(interval);
+        }
+    }, 20)
+}   
+
+function generateUniqueId() {
+    const timestamp = Date.now();
+    const randomNumber = Math.random();
+    const hexadecimalString = randomNumber.toString(16);
+
+    return `id-${timestamp}-${hexadecimalString}`;
+}
+
+function chatStripe (isAi, value , uniqueId) {
+    return (
+        `
+        <div class="wrapper ${isAi && 'ai'}" >
+           <div class="chat">
+             <div className="profile">
+               <img 
+                src="${isAi ? bot : user}"    
+                alt="${isAi ? 'bot' : 'user'}"
+                />
+            </div>
+            <div class="message" id ${uniqueId} > ${value}</div> 
+        </div>
+        </div>    
+         
+        `
+    )
+}
+
